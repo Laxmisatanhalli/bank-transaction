fbba7b69582c5c17b6e8f5857f38b12937550b96
Laxmi Satanhalli <lsatenhalli@gmail.com>
2026-07-15 08:48:13 +0530

    fix: replace Gmail OAuth2/SMTP with Mailtrap API for email service

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,44 +1,32 @@
 require('dotenv').config();
-const nodemailer = require('nodemailer');
+const { MailtrapClient } = require("mailtrap");
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    type: 'OAuth2',
-    user: process.env.EMAIL_USER,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken: process.env.REFRESH_TOKEN,
-  },
+const client = new MailtrapClient({
+  token: process.env.MAILTRAP_TOKEN,
 });
 
-// Verify the connection configuration
-transporter.verify((error, success) => {
-  if (error) {
-    console.error('Error connecting to email server:', error);
-  } else {
-    console.log('Email server is ready to send messages');
-  }
-});
+const sender = {
+  email: process.env.MAILTRAP_SENDER_EMAIL,
+  name: "Backend Ledger",
+};
 
 // Function to send email
 const sendEmail = async (to, subject, text, html) => {
   try {
-    const info = await transporter.sendMail({
-      from: `"Backend Ledger" <${process.env.EMAIL_USER}>`, // sender address
-      to, // list of receivers
-      subject, // Subject line
-      text, // plain text body
-      html, // html body
+    const info = await client.send({
+      from: sender,
+      to: [{ email: to }],
+      subject,
+      text,
+      html,
+      category: "Backend Ledger",
     });
 
-    console.log('Message sent: %s', info.messageId);
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    console.log('Message sent:', info);
   } catch (error) {
     console.error('Error sending email:', error);
   }
 };
-
 
 async function sendRegistrationEmail(userEmail, name) {
   const subject = 'Welcome to Backend Ledger!';
@@ -64,6 +52,4 @@ async function sendTransactionFailureEmail(userEmail, name, amount, toAccount) {
   await sendEmail(userEmail, subject, text, html);
 }
 
-module.exports = { sendRegistrationEmail, 
-  sendTransactionEmail,
-   sendTransactionFailureEmail };
+module.exports = { sendRegistrationEmail, sendTransactionEmail, sendTransactionFailureEmail };
